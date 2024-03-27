a093309282a63ae4343f9fc029bb173a108d3b09
Gleb Akunenko <akunenko.study@gmail.com>
2024-03-28 02:31:45 +0300

    Add notify

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
 from src.router import router
 
+import src.config as config
+import requests
+
 app = FastAPI(
     title="Tochka Interesa Token Server"
 )
 
 app.include_router(router)
+
+@app.on_event("startup")
+async def start_up():
+    try:
+        requests.post(config.start_request)
+    except:
+        pass
+
+
+
+@app.on_event("shutdown")
+def on_shutdown():
+    try:
+        requests.post(config.stop_request)
+    except:
+        pass
